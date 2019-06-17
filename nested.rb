e8c777fb421c2cc06569df01e4cd4f72e89d70ec
Samantha Kane <s.kane133@gmail.com>
2019-06-17 01:34:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,7 +126,7 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-        programmer_hash[:alan_kay][:known_for] = "GUI"
      }
+     programmer_hash[:alan_kay][:known_for] = "GUI"
 
 end
